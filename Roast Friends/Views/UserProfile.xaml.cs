ee4f485aa3864571e0633dfc5a957ab1db95ae06
Patrixon <patryk1gorcz@gmail.com>
2024-03-05 11:42:27 +0100

    dodanie logiki wylogowania

--- Roast Friends/Views/UserProfile.xaml.cs
+++ Roast Friends/Views/UserProfile.xaml.cs
@@ -1,9 +1,17 @@
+using Roast_Friends.Other;
 namespace Roast_Friends.Views;
 
 public partial class UserProfile : ContentPage
 {
-	public UserProfile()
-	{
-		InitializeComponent();
-	}
+    public UserProfile()
+    {
+        InitializeComponent();
+    }
+
+    private async void OnLogoutClicked(object sender, EventArgs e)
+    {
+        await SecureStorage.SetAsync("auth_token", string.Empty);
+        Settings.isLoggedIn = false;
+        await Shell.Current.GoToAsync("///StartPage");
+    }
 }
