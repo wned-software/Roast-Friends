c6a3ba107d3041d3a92779585977a46ff044d007
paeqw <paeqw12@gmail.com>
2024-03-05 09:23:34 +0100

    login logic?

--- Roast Friends/Views/Login/LogInFormView.xaml.cs
+++ Roast Friends/Views/Login/LogInFormView.xaml.cs
@@ -1,9 +1,30 @@
+using Firebase.Auth;
+
 namespace Roast_Friends.Views.Login;
 
 public partial class LogInFormView : ContentPage
 {
-	public LogInFormView()
-	{
-		InitializeComponent();
-	}
+    private FirebaseAuthClient _authClient;
+    public LogInFormView(FirebaseAuthClient authClient)
+    {
+        InitializeComponent();
+        _authClient = authClient;
+    }
+
+    private async void LoginButton_Clicked(object sender, EventArgs e)
+    {
+        string email = EmailEntry.Text;
+        string password = PasswordEntry.Text;
+
+        try
+        {
+            var userCredential = await _authClient.SignInWithEmailAndPasswordAsync(email, password);
+            await DisplayAlert("Success", "Login successful.", "OK");
+
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Error", ex.Message, "OK");
+        }
+    }
 }
